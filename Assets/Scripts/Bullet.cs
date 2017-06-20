3a1332349088d9fcec50bf385488470dfa0d5994
zyouyowa <transam3y@gmail.com>
2017-06-20 18:44:15 +0900

    convert tabs to spaces 💢

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -3,11 +3,11 @@
 public class Bullet : MonoBehaviour {
     public float speed = 10f;
     private Rigidbody2D rigidbody2d;
-	void Start () {
+    void Start () {
         rigidbody2d = GetComponent<Rigidbody2D>();
-	}
-	
-	void Update () {
+    }
+        
+    void Update () {
         rigidbody2d.velocity = transform.up * speed;
-	}
+    }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,10 +5,10 @@ public class Player : MonoBehaviour {
     public float speed = 5f;
     public GameObject bullet;
     private Rigidbody2D rigidbody2d;
-	void Start () {
+    void Start () {
         rigidbody2d = GetComponent<Rigidbody2D>();
         StartCoroutine(StartShot());
-	}
+    }
 
     IEnumerator StartShot(){
         WaitForSeconds wait = new WaitForSeconds(0.05f);
@@ -17,11 +17,11 @@ public class Player : MonoBehaviour {
             yield return wait;
         }
     }
-	
-	void Update () {
+        
+    void Update () {
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
         Vector2 direction = new Vector2(x, y).normalized;
         rigidbody2d.velocity = direction * speed;
-	}
+    }
 }
