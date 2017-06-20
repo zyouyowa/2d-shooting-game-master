dff0745c87d92a01eec5234b477cfd7a025ffce2
zyouyowa <transam3y@gmail.com>
2017-06-20 18:39:37 +0900

    test commit for the endings of lines
    改行コードが治ってるかの確認です…

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,1 +1,13 @@
-﻿using UnityEngine;public class Bullet : MonoBehaviour {    public float speed = 10f;    private Rigidbody2D rigidbody2d;	void Start () {        rigidbody2d = GetComponent<Rigidbody2D>();	}		void Update () {        rigidbody2d.velocity = transform.up * speed;	}}
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+    public float speed = 10f;
+    private Rigidbody2D rigidbody2d;
+	void Start () {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update () {
+        rigidbody2d.velocity = transform.up * speed;
+	}
+}
