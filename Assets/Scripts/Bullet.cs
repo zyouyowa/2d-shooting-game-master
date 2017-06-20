513188751918df010454aaed67d986eae4696f5e
zyouyowa <transam3y@gmail.com>
2017-06-20 18:24:17 +0900

    init
    チュートリアルの第3回までの内容をやった

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,1 @@
+﻿using System.Collections;using System.Collections.Generic;using UnityEngine;public class Bullet : MonoBehaviour {    public float speed = 10f;    private Rigidbody2D rigidbody2d;	void Start () {        rigidbody2d = GetComponent<Rigidbody2D>();	}		void Update () {        rigidbody2d.velocity = transform.up * speed;	}}
