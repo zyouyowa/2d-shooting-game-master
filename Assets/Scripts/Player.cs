52c18daec2dc481592ebe32a41d40e5a370c9c0f
zyouyowa <transam3y@gmail.com>
2017-06-20 18:54:26 +0900

    code formatted💢

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,23 +5,24 @@ public class Player : MonoBehaviour {
     public float speed = 5f;
     public GameObject bullet;
     private Rigidbody2D rigidbody2d;
+
     void Start () {
-        rigidbody2d = GetComponent<Rigidbody2D>();
-        StartCoroutine(StartShot());
+        rigidbody2d = GetComponent<Rigidbody2D> ();
+        StartCoroutine (StartShot ());
     }
 
-    IEnumerator StartShot(){
-        WaitForSeconds wait = new WaitForSeconds(0.05f);
-        while(true){
-            Instantiate(bullet, transform.position, transform.rotation);
+    IEnumerator StartShot () {
+        WaitForSeconds wait = new WaitForSeconds (0.05f);
+        while (true) {
+            Instantiate (bullet, transform.position, transform.rotation);
             yield return wait;
         }
     }
-        
+
     void Update () {
-        float x = Input.GetAxisRaw("Horizontal");
-        float y = Input.GetAxisRaw("Vertical");
-        Vector2 direction = new Vector2(x, y).normalized;
+        float x = Input.GetAxisRaw ("Horizontal");
+        float y = Input.GetAxisRaw ("Vertical");
+        Vector2 direction = new Vector2 (x, y).normalized;
         rigidbody2d.velocity = direction * speed;
     }
 }
